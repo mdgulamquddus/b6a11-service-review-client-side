3b0117fad714374e93174eab267087cccaef0e52
md-gulam-quddus <sohag.1984@gmail.com>
2022-12-17 18:16:13 +0100

    env.local file created and env variable added

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,9 +1,20 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../../src/firebase/firebase.init";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
-  const authInfo = { displayName: "akash" };
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+  const authInfo = { user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
